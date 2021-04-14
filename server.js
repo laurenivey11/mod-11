221170eb6e50f9913f7f287cd23fc9f42488e598
Lauren Ivey <lauren.ivey11@gmail.com>
2021-04-13 21:19:46 -0400

    second commit

---
+++ server.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const { notes } = require("./db/db.json");
+const app = express();
+
+app.get('/api/notes', (req,res) => {
+    res.send('Hello!');
+});
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
